f6294ed72907c6941518b4caa3aefd357499453a
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2023-07-08 23:21:48 +0400

    Test
    On a testé de nouvelles façon d'utiliser le modèle avec streamlit.
    Pour le moment en utilisant la commande "py uber_pickups.py" le code fonctionne parfaitement en local

--- backend/uber_pickups.py
+++ backend/uber_pickups.py
@@ -1,44 +1,29 @@
-import numpy as np, streamlit as st, pandas as pd
+import streamlit as st
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 
 # Titre de l'application
-st.title('Ramassages Uber à New York')
+st.title("Détection d'intrusion")
+st.subheader('Activer votre webcam, pour commencer')
 
-# Définition des constantes
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+if st.button('Commencer'):
+    # Création du modèle YOLO
+    model = YOLO('./yolov8n.pt')
+    
+    # Détection et affichage des résultats
+    results = st.camera_input("Détection en cours")
+    while st.camera_input:
+        model(results, source="0", stream=True)
 
-# Fonction pour charger les données (mise en cache)
+    
+# if picture:
+#     st.image(picture)
 
+# model = YOLO('./yolov8n.pt')
+# results = model.predict(source="0", show=True, stream=True)
+# for r in results:
+#     boxes = r.boxes  # Boxes object for bbox outputs
+#     masks = r.masks  # Masks object for segment masks outputs
+#     probs = r.probs  # Class probabilities for classification outputs
+# print(results)
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    def lowercase(x): return str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-
-# Chargement des données
-data_load_state = st.text('Chargement des données')
-data = load_data(10001)
-data_load_state.text("Données Chargées !")
-
-# Affichage des données brutes (optionnel)
-if st.checkbox('Montrer les données brutes'):
-    st.write(data)
-
-# Affichage du nombre de trajets par heure (histogramme)
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
-st.subheader('Nombre de ramassages par heure')
-st.bar_chart(hist_values)
-
-# Filtre des données en fonction de l'heure choisie
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-# Affichage de la carte des trajets pour une heure spécifique
-st.subheader(f'Carte des ramassages {hour_to_filter}:00')
-st.map(filtered_data)
